6e6326b5ead0bd206de31e8d74a019f20e2077ad
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2025-10-09 14:45:23 -0300

    feat(Init):First commit

---
+++ app/src/main/java/br/com/wgc/favoritelink/home_screen/HomeScreen.kt
@@ -0,0 +1,9 @@
+package br.com.wgc.favoritelink.home_screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
